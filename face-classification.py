2838eb38b753998f6939f6049799d769723dcab8
YuSeungjun <dbtmdwns63@naver.com>
2023-06-15 05:17:22 +0900

    최종

--- face-classification.py
+++ face-classification.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 from __future__ import print_function
 import face_recognition
 import cv2
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 import numpy as np
 import os
 import pickle
@@ -35,7 +34,22 @@ class FaceClustering():
         # Draw a box around the face
         for face in faces_in_frame:
             (top, right, bottom, left) = face.box
+            width = right - left
+            height = bottom - top
+            left = max(0, left - int(width * 0.4))  # 얼굴 면적 40% 넓게 감지됨
+            top = max(0, top - int(height * 0.4))  # 얼굴 면적 40% 넓게 감지됨
+            right = min(frame.shape[1], right + int(width * 0.4))  # 얼굴 면적 40% 넓게 감지됨
+            bottom = min(frame.shape[0], bottom + int(height * 0.4))
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    def cluster_faces(self, n_clusters=12):
+        encodings = [face.encoding for face in self.faces]
+        if len(encodings) >= n_clusters:
+            kmeans = KMeans(n_clusters=n_clusters, n_jobs=-1)
+            labels = kmeans.fit_predict(encodings)
+
+            for i, face in enumerate(self.faces):
+                face.name = str(labels[i])  # assign cluster id as name
 
     def encode(self, src_file, capture_per_second, stop=0):
         src = cv2.VideoCapture(src_file)
@@ -60,6 +74,7 @@ class FaceClustering():
         if not os.path.exists(self.capture_dir):
             os.mkdir(self.capture_dir)
 
+        individual_face_count = {}
         self.run_encoding = True
         while self.run_encoding:
             ret, frame = src.read()
@@ -87,14 +102,27 @@ class FaceClustering():
                 face = Face(frame_id, None, box, encoding)
                 faces_in_frame.append(face)
 
-            # save the frame
-            self.drawBoxes(frame, faces_in_frame)
-            pathname = os.path.join(self.capture_dir,
-                                    self.capture_filename(frame_id))
-            cv2.imwrite(pathname, frame)
-            #cv2.imshow("Frame", frame)
-
             self.faces.extend(faces_in_frame)
+
+            # Cluster the faces
+            self.cluster_faces()
+
+            # save the frame with drawn boxes
+            self.drawBoxes(frame, faces_in_frame)
+            
+            # save individual faces
+            for face in faces_in_frame:
+                face_dir = os.path.join(self.capture_dir, f"cluster_{face.name}")
+                if not os.path.exists(face_dir):
+                    os.mkdir(face_dir)
+
+                # Save box_frame in the same directory as individual faces
+                box_frame_path = os.path.join(face_dir, f"frame_{frame_id:08d}_boxes.jpg")
+                cv2.imwrite(box_frame_path, frame)
+
+                face_path = os.path.join(face_dir, f"face_{frame_id:08d}.jpg")
+                face_image = frame[face.box[0]:face.box[2], face.box[3]:face.box[1]]
+                cv2.imwrite(face_path, face_image)
 
         # restore SIGINT (^C) handler
         signal.signal(signal.SIGINT, prev_handler)
@@ -110,59 +138,6 @@ class FaceClustering():
         with open(filename, "rb") as f:
             data = f.read()
             self.faces = pickle.loads(data)
-
-    def getFaceImage(self, image, box):
-        img_height, img_width = image.shape[:2]
-        (top, right, bottom, left) = box
-        box_width = right - left
-        box_height = bottom - top
-        top = max(top - box_height, 0)
-        bottom = min(bottom + box_height, img_height - 1)
-        left = max(left - box_width, 0)
-        right = min(right + box_width, img_width - 1)
-        return image[top:bottom, left:right]
-
-    def cluster(self):
-        if len(self.faces) is 0:
-            print("no faces to cluster")
-            return
-
-        print("start clustering %d faces..." % len(self.faces))
-        encodings = [face.encoding for face in self.faces]
-
-        # cluster the embeddings
-        clt = DBSCAN(metric="euclidean")
-        clt.fit(encodings)
-
-        # determine the total number of unique faces found in the dataset
-        label_ids = np.unique(clt.labels_)
-        num_unique_faces = len(np.where(label_ids > -1)[0])
-        print("clustered %d unique faces." % num_unique_faces)
-
-        os.system("rm -rf ID*")
-        for label_id in label_ids:
-            dir_name = "ID%d" % label_id
-            os.mkdir(dir_name)
-
-            # find all indexes of label_id
-            indexes = np.where(clt.labels_ == label_id)[0]
-
-            # save face images
-            for i in indexes:
-                frame_id = self.faces[i].frame_id
-                box = self.faces[i].box
-                pathname = os.path.join(self.capture_dir,
-                                        self.capture_filename(frame_id))
-                image = cv2.imread(pathname)
-                face_image = self.getFaceImage(image, box)
-                filename = dir_name + "-" + self.capture_filename(frame_id)
-                pathname = os.path.join(dir_name, filename)
-                cv2.imwrite(pathname, face_image)
-
-            print("label_id %d" % label_id, "has %d faces" % len(indexes),
-                  "in '%s' directory" % dir_name)
-
-        print('clustering done')
 
 
 if __name__ == '__main__':
@@ -185,10 +160,3 @@ if __name__ == '__main__':
             src_file = 0
         fc.encode(src_file, args.capture, args.stop)
         fc.save("encodings.pickle")
-
-    try:
-        fc.load("encodings.pickle")
-    except FileNotFoundError:
-        print("No or invalid encoding file. Encode first using -e flag.")
-        exit(1)
-    fc.cluster()
